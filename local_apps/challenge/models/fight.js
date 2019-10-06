4b002cb3277b6a02803e7f9c573e392a5aa2ebcc
Stanila Andrei <andrei133303@gmail.com>
2019-10-06 11:06:07 +0300

    Challenge routes

--- local_apps/challenge/models/fight.js
+++ local_apps/challenge/models/fight.js
@@ -2,6 +2,9 @@ const Sequelize = require('sequelize')
 const { db } = require('../index')
 
 const Fight = db.define('fights', {
+  fightId: {
+    type: Sequelize.INTEGER
+  },
   challengerFinished: {
     type: Sequelize.BOOLEAN
   },
